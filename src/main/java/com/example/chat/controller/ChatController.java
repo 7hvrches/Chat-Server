4ae1b39b2d20847f35a8e43121b0859d47038b9e
7hvrches <7hvrches@naver.com>
2023-12-21 18:11:33 +0900

    .

--- src/main/java/com/example/chat/controller/ChatController.java
+++ src/main/java/com/example/chat/controller/ChatController.java
@@ -1,26 +1,26 @@
 package com.example.chat.controller;
 
-import com.example.chat.dto.ChatRoom;
-import com.example.chat.service.ChatService;
+import com.example.chat.dto.ChatMessage;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Controller;
 
-import java.util.List;
-
+/*
+    @MessageMapping 메세지 발행 처리
+    client 에서 prefix 를 붙여 /pub/chat/message 로 발행 요청 시 ChatController 가 해당 메시지를 받아 처리
+    발행되면 메시지를 send
+ */
 @RequiredArgsConstructor
-@RestController
-@RequestMapping("/chat")
+@Controller
 public class ChatController {
 
-    private final ChatService chatService;
+    private final SimpMessageSendingOperations messagingTemplate;
 
-    @PostMapping
-    public ChatRoom createRoom(@RequestParam(name = "name") String name) {
-        return chatService.createRoom(name);
-    }
-
-    @GetMapping
-    public List<ChatRoom> findAllRoom() {
-        return chatService.findAllRoom();
+    @MessageMapping("/chat/message")
+    public void message(ChatMessage message) {
+        if(ChatMessage.MessageType.ENTER.equals(message.getType()))
+            message.setMessage(message.getSender() + "님이 입장하셨습니다.");
+        messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
     }
 }
