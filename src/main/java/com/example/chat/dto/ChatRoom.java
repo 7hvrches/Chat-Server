b541796ec5a894aefecc2af316cba33722085dcf
7hvrches <7hvrches@naver.com>
2023-12-28 04:17:06 +0900

    ChatRoom Serialize

--- src/main/java/com/example/chat/dto/ChatRoom.java
+++ src/main/java/com/example/chat/dto/ChatRoom.java
@@ -1,11 +1,19 @@
 package com.example.chat.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 
+import java.io.Serializable;
 import java.util.UUID;
 
+/**
+ * Redis에 저장되는 객체들은 Serialize 가능해야 하므로 Serializable 구현
+ */
 @Getter
-public class ChatRoom {
+@Setter
+public class ChatRoom implements Serializable {
+
+    private static final long serialVersionUID = 6494678977089006639L;
 
     private String roomId;
     private String name;
