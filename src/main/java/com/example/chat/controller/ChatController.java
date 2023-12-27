615622712f59cd58f56da76f9af937695b466d66
7hvrches <7hvrches@naver.com>
2023-12-28 04:15:29 +0900

    .

--- src/main/java/com/example/chat/controller/ChatController.java
+++ src/main/java/com/example/chat/controller/ChatController.java
@@ -1,26 +1,34 @@
 package com.example.chat.controller;
 
 import com.example.chat.dto.ChatMessage;
+import com.example.chat.redis.RedisPublisher;
+import com.example.chat.repository.ChatRoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 
-/*
-    @MessageMapping 메세지 발행 처리
-    client 에서 prefix 를 붙여 /pub/chat/message 로 발행 요청 시 ChatController 가 해당 메시지를 받아 처리
-    발행되면 메시지를 send
+/**
+ *  @MessageMapping 메세지 발행 처리
+ *  클라이언트가 채팅방 입장 시 Redis 채팅방에서 대화가 가능하도록 리스너를 연동하는 enterChatRoom 메서드 설정
+ *  채팅방에 발행된 메시지는 서로 다른 서버에 공유하기 위해 redis의 채팅방으로 발행합니다
  */
 @RequiredArgsConstructor
 @Controller
 public class ChatController {
 
-    private final SimpMessageSendingOperations messagingTemplate;
+    private final RedisPublisher redisPublisher;
+    private final ChatRoomRepository chatRoomRepository;
 
+    /**
+     * websocket "/pub/chat/message"로 들어오는 메시징을 처리한다.
+     */
     @MessageMapping("/chat/message")
     public void message(ChatMessage message) {
-        if(ChatMessage.MessageType.ENTER.equals(message.getType()))
+        if (ChatMessage.MessageType.ENTER.equals(message.getType())) {
+            chatRoomRepository.enterChatRoom(message.getRoomId());
             message.setMessage(message.getSender() + "님이 입장하셨습니다.");
-        messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
+        }
+        // Websocket에 발행된 메시지를 redis로 발행한다(publish)
+        redisPublisher.publish(chatRoomRepository.getTopic(message.getRoomId()), message);
     }
 }
