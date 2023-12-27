08960d810980207301c7c4160aac91ca24440191
7hvrches <7hvrches@naver.com>
2023-12-28 04:15:52 +0900

    .

--- src/main/java/com/example/chat/config/WebSockConfig.java
+++ src/main/java/com/example/chat/config/WebSockConfig.java
@@ -1,12 +1,12 @@
-package com.example.chat;
+package com.example.chat.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
 
-/*
-    pub/sub 메시징 구현을 위해 메시지를 발행하는 요청의 prefix 는 /pub 로 시작하도록 설정
-    메시지를 구독하는 요청의 prefix 는 /sub 로 시작하도록 설정
+/**
+ *  pub/sub 메시징 구현을 위해 메시지를 발행하는 요청의 prefix 는 /pub 로 시작하도록 설정
+ *  메시지를 구독하는 요청의 prefix 는 /sub 로 시작하도록 설정
  */
 @Configuration
 @EnableWebSocketMessageBroker //Stomp 사용을 위한 어노테이션
