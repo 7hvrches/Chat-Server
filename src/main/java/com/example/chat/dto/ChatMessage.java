5cf39584dcff7cd10505105f9aa5dd8e02999573
7hvrches <7hvrches@naver.com>
2023-12-30 16:09:43 +0900

    userCount 및 메시지 타입 QUIT 추가

--- src/main/java/com/example/chat/dto/ChatMessage.java
+++ src/main/java/com/example/chat/dto/ChatMessage.java
@@ -1,5 +1,6 @@
 package com.example.chat.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,13 +8,26 @@ import lombok.Setter;
 @Setter
 public class ChatMessage {
 
-    //입장, 채팅
+    //입장, 채팅, 퇴장
     public enum MessageType {
-        ENTER, TALK
+        ENTER, TALK, QUIT
     }
 
     private MessageType type; //메시지 타입
     private String roomId; //방번호
     private String sender; //메시지 보낸 사람
     private String message; //메시지
+    private long userCount; //채팅방 인원수, 채팅방 내에서 메시지가 전달될 때 인원수 갱신시 사용
+
+    public ChatMessage() {
+
+    }
+
+    @Builder
+    public ChatMessage(MessageType type, String roomId, String sender, String message) {
+        this.type = type;
+        this.roomId = roomId;
+        this.sender = sender;
+        this.message = message;
+    }
 }
